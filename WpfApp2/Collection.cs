4bdd0c7ef345e61e8966c8ed78a34a2ef757cdda
ZI-TING ZHUANG <0976.545.421abc@gmail.com>
2021-01-07 17:18:16 +0800

    全部課程資料讀取完成

--- WpfApp2/Collection.cs
+++ WpfApp2/Collection.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using CsvHelper;
 using CsvHelper.Configuration.Attributes;
 
+
 namespace WpfApp2
 {
     class Student
@@ -14,5 +15,29 @@ namespace WpfApp2
         public string ID { get; set; }
         [Index(1)]
         public string Name { get; set; }
+        public override string ToString()
+        {
+            return $"{this.ID} {this.Name}";
+        }
     }
+    class AllFile //選課詳細表
+    {
+        [Index(0)]
+        public string TeacherName { get; set; } //教師名稱
+        [Index(1)]
+        public string CourseName { get; set; } //課程名稱
+        [Index(2)]
+        public string Point { get; set; } //學分
+        [Index(3)]
+        public string Type { get; set; } //必、選修
+        [Index(4)]
+        public string OpeningClass { get; set; }//開課班級
+        [Index(5)]
+        public string ClassTime { get; set; } //上課日期
+        public override string ToString()
+        {
+            return $"{this.TeacherName} {this.CourseName} {this.Point} {this.Type} {this.OpeningClass} {this.ClassTime}";
+        }
+    }
+
 }
--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -22,25 +22,40 @@ namespace WpfApp2
     public partial class MainWindow : Window
     {
         List<Student> studentlist = new List<Student>();
-        ObservableCollection<string> studentNames = new ObservableCollection<string>();
+        List<AllFile> teacherFileslist = new List<AllFile>();
         public MainWindow()
         {
             InitializeComponent();
-            using (var reader = new StreamReader("D:\\2B.csv",Encoding.Default))
+            OpenFileInListStudent();//讀入學生檔案
+            OpenFileInListAllTeacher();//讀入課程資料
+        }
+
+        private void OpenFileInListAllTeacher()
+        {
+            using (var reader = new StreamReader("D:\\course.csv", Encoding.Default))
+            using (var csv = new CsvReader(reader, CultureInfo.CurrentCulture))
+            {
+                var records = csv.GetRecords<AllFile>();
+                foreach (var Allfile in records)
+                {
+                    teacherFileslist.Add(Allfile);
+                }
+            }
+            AllClassList.ItemsSource = teacherFileslist;
+        }
+
+        private void OpenFileInListStudent()
+        {
+            using (var reader = new StreamReader("D:\\2B.csv", Encoding.Default))
             using (var csv = new CsvReader(reader, CultureInfo.CurrentCulture))
             {
                 var records = csv.GetRecords<Student>();
-                foreach(var studentfile in records)
+                foreach (var studentfile in records)
                 {
                     studentlist.Add(studentfile);
-                    studentNames.Add(studentfile.ID+studentfile.Name);
-                    
                 }
             }
-            CBStudent.ItemsSource = studentNames;
-            studentNames.Clear();
+            CBStudent.ItemsSource = studentlist;
         }
-
-
     }
 }
